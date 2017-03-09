43fe3f14564a61bcfabd63653582bbe55ecdf7b1
y_otsuka <otsuka.main@gmail.com>
2017-03-09 09:31:41 +0000

    feat: 拡張子が大文字のファイルも許可

--- imagen/common/util.py
+++ imagen/common/util.py
@@ -12,7 +12,7 @@ def is_allowed_image(image: FileStorage) -> bool:
     # if os.path.splitext(image.filename)[1] not in current_app.config['ALLOWED_EXTENSIONS']:
     # 上の場合拡張子は '.png' になる
     # 下の場合拡張子は 'png' になる
-    if image.filename.rsplit('.', 1)[1] not in current_app.config['ALLOWED_EXTENSIONS']:
+    if image.filename.rsplit('.', 1)[1].lower() not in current_app.config['ALLOWED_EXTENSIONS']:
         return False
 
     if im.format not in current_app.config['ALLOWED_FORMATS']:
